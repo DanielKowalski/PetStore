ba58b6a5fe576aa8ec079ed3497625d57a3dd1d2
Daniel Kowalski <danielk.biznes@gmail.com>
2018-09-17 21:01:27 +0200

    Add controller to greeting customer with name

--- src/me/daniel/controllers/HelloController.java
+++ src/me/daniel/controllers/HelloController.java
@@ -2,6 +2,7 @@ package me.daniel.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,6 +12,15 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String hello(ModelMap model) {
 		model.addAttribute("greetings", "Witaj drogi kliencie!");
+		return "hello";
+	}
+	
+	@RequestMapping(path = "/{name}", method = RequestMethod.GET)
+	public String helloName(@PathVariable String name, ModelMap model) {
+		StringBuilder builder = new StringBuilder(name.toLowerCase());
+		builder.setCharAt(0, Character.toUpperCase(builder.charAt(0)));
+		name = builder.toString();
+		model.addAttribute("greetings", "Witaj " + name + "!");
 		return "hello";
 	}
 	
