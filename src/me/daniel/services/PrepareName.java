945b506bc3a2358d3f348f1a335bface163ba06a
Daniel Kowalski <danielk.biznes@gmail.com>
2018-09-18 17:54:11 +0200

    Move capitalizing to separate method

--- src/me/daniel/services/PrepareName.java
+++ src/me/daniel/services/PrepareName.java
@@ -8,6 +8,11 @@ public class PrepareName {
 	public String prepareName(String name) {
 		//TODO Dodać obsługę nazwisk
 		name = removeNotLettersFrom(name);
+		name = capitalize(name);
+		return name;
+	}
+	
+	public String capitalize(String name) {
 		StringBuilder builder = new StringBuilder(name.toLowerCase());
 		builder.setCharAt(0, Character.toUpperCase(builder.charAt(0)));
 		return builder.toString();
