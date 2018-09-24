92b48828610081d14dbd34b4845433c61e11fbe6
Daniel Kowalski <danielk.biznes@gmail.com>
2018-09-24 22:18:05 +0200

    Add error page

--- WebContent/WEB-INF/views/errorView.jsp
+++ WebContent/WEB-INF/views/errorView.jsp
@@ -1,12 +1,26 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Andrew's PetStore- błąd</title>
+	<style>
+		.main {
+			color: #F00;
+			background-color: #A00;
+			border: 2px solid #700;
+			text-align: center;
+		}
+	</style>
 </head>
 <body>
-	<font color="red"><b>Wystąpił błąd!</b></font>
+	<div class="main">
+		<h2>${title}</h2>
+	</div>
+	<c:forEach items="${errorsList}" var="error">
+		${error} <br />
+	</c:forEach>
 </body>
 </html>
--- src/me/daniel/controllers/AnimalController.java
+++ src/me/daniel/controllers/AnimalController.java
@@ -1,10 +1,13 @@
 package me.daniel.controllers;
 
+import java.util.ArrayList;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +28,17 @@ public class AnimalController {
 	public String newAnimal(@Valid @ModelAttribute("animal")
 		Animal animal, BindingResult result, ModelMap model) {
 		if(result.hasErrors()) {
-			
+			ArrayList<String> errorsList;
+			errorsList = new ArrayList<String>();
+			for(FieldError error : 
+				result.getFieldErrors()) {
+				errorsList.add(error.getObjectName() + ": "
+						+ error.getField() + ": "
+						+ error.getDefaultMessage());
+			}
+			model.addAttribute("errorsList", errorsList);
+			model.addAttribute("title", 
+					"Wystąpiły następujące błędy!!!");
 			return "error";
 		}
 		model.addAttribute("title", 
